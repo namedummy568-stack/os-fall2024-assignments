7770b35cd6bbb9bb14aa277faeff29a40c8e0305
namedummy568-stack <namedummy568@gmail.com>
2025-09-21 17:31:03 +0530

    Initial commit for Assignment 1 kernel module

---
+++ assignment1/kernel_module.c
@@ -0,0 +1,22 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+static int __init hello_init(void)
+{
+    printk(KERN_INFO "Hello, Kernel Module!\n");
+    return 0;
+}
+
+static void __exit hello_exit(void)
+{
+    printk(KERN_INFO "Goodbye, Kernel Module!\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Your Name");
+MODULE_DESCRIPTION("A simple kernel module.");
+MODULE_VERSION("0.1");
